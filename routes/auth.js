d9052b1190c4feb2a5bf36b50cda1ce917c8bf4a
Ephantus Okumu <ephantus.okumu@interintel.co.ke>
2020-04-06 10:43:10 +0300

    auth middleware/protect routes

--- routes/auth.js
+++ routes/auth.js
@@ -5,13 +5,19 @@ const jwt = require('jsonwebtoken');
 const config = require('config');
 const {check, validationResult} = require('express-validator/check');
 const User = require('../models/User');
-
+const auth = require('../middleware/auth')
 
 //@ route GET api/auth
 //@desc  Get logged in users
 //@access  Privaatee
-router.get('/', (req, res) =>{
-    res.send('Get logged in users');
+router.get('/', auth, async(req, res) =>{
+   try {
+       const user = await User.findById(req.user.id).select('-password');
+       res.json(user);
+   } catch (err) {
+       console.error(err.message);  
+       res.status(500).send('SERVER ERROR') 
+   }
 });
 
 //@ route POST api/auth
