22d2c21c1cf0d2818519b9142cd60472448fde8b
Ephantus Okumu <ephantus.okumu@interintel.co.ke>
2020-04-08 16:41:47 +0300

    contact state and context

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,66 @@
+import React, {useReducer} from 'react'
+import uuid from 'uuid'
+import ContactContext from './contactContext'
+import contactReducer from './contactReducer';
+import {
+    ADD_CONTACT,
+    DELETE_CONTACT,
+    SET_CURRENT,
+    CLEAR_CURRENT,
+    UPDATE_CONTACT,
+    FILTER_CONTACTS,
+    CLEAR_FILTER
+} from '../types';
+   
+const ContactState = props =>{
+    const initialState ={
+        contacts: [
+           {
+               id:1,
+               name: 'Ephantufs Okumu',
+               email: 'eohnet@gmail.com',
+               phone: '+25471566336467',
+               type: 'personal'
+           },
+           {
+            id:2,
+            name: 'Ephantuccs Okumu',
+            email: 'eohnet@gmail.com',
+            phone: '+25471566336467',
+            type: 'personal'
+        }, {
+            id:3,
+            name: 'Ephaccntus Okumu',
+            email: 'eohnet@gccmail.com',
+            phone: '+2547156cc6336467',
+            type: 'profesional'
+        }
+
+        ]
+    };
+ const [state, dispatch] = useReducer(contactReducer, initialState);
+
+ //add contacts
+
+ //delete contacts
+
+ //set current cotnatc
+
+ //clear
+
+ //update
+
+ //filter contacts
+
+
+ return (
+     <ContactContext.Provider 
+     value={{
+         contacts:state.contacts
+     }}>
+    {props.children}
+     </ContactContext.Provider>
+ )
+};
+
+export default ContactState;
